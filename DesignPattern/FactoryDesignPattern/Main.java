cfefcb701b0ce72d15190d4c5a5501806f538ef5
Abhi2505 <47726141+Abhi2505@users.noreply.github.com>
2024-08-04 19:09:39 +0530

    Update Main.java

--- DesignPattern/FactoryDesignPattern/Main.java
+++ DesignPattern/FactoryDesignPattern/Main.java
@@ -3,7 +3,7 @@ package DesignPattern.FactoryDesignPattern;
 public class Main {
 	public static void main(String[] args) {
 		vehicleManufacturingFactory f1=new vehicleManufacturingFactory();
-		VehicleManufacture carManufacture=f1.getVehicle("CarManufacture");
-		VehicleManufacture bikeManufacture=f1.getVehicle("BikeManufacture");
+		VehicleManufacture carManufacture=f1.getVehicleManufacture("CarManufacture");
+		VehicleManufacture bikeManufacture=f1.getVehicleManufacture("BikeManufacture");
 	}
 }
